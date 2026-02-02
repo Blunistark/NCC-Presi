c8182135b5a0e53e39f8324980962dfbdefb2591
Meghnath <meghnath1782004@gmail.com>
2026-02-02 09:22:28 +0530

    feat: add ANO dashboard page.

---
+++ frontend/src/app/(DashboardLayout)/ano/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import DashboardView from '@/app/(DashboardLayout)/components/dashboard/DashboardView';
+
+const AnoDashboard = () => {
+    return <DashboardView role="ANO" />;
+}
+
+export default AnoDashboard;
